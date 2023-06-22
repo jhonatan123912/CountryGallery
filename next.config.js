3f72a3430323a77382b71400e7e2a07bd8ead256
jhonatan123912 <jhonatanedward55@gmail.com>
2023-06-22 10:52:13 +0700

    deploy vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
+
+images: {
+  domains: [
+            'source.unsplash.com',
+            'piscum.photos',
+            'images.unsplash.com'
+           ]
+}
+}
 module.exports = nextConfig
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,12 +2,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  name: string,
+  first_name : string, 
+  last_name : string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe', first_name: 'Jhonatan', last_name: 'Sitorus' })
 }
